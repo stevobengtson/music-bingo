3b5b673f74f570a4fc8bdca25a9473556cea44e4
Steven Bengtson <steven.bengtson@me.com>
2020-05-02 17:00:41 -0700

    Add comments to script

--- build_and_push.sh
+++ build_and_push.sh
@@ -2,11 +2,22 @@
 
 version=1.0.4
 
+echo "Building containers..."
+echo "Building web..."
 docker build -t stevobengtson/music-bingo-web:$version ./web
+echo "Building api..."
 docker build -t stevobengtson/music-bingo-api:$version ./api
 
+echo "Tagging containers to latest..."
+echo "Tagging web container to latest..."
 docker tag stevobengtson/music-bingo-web:$version stevobengtson/music-bingo-web:latest
+echo "Tagging api container to latest..."
 docker tag stevobengtson/music-bingo-api:$version stevobengtson/music-bingo-api:latest
 
+echo "Pushing containers to docker hub..."
+echo "Pushing web container to docker hub..."
 docker push stevobengtson/music-bingo-web:latest
+echo "Pushing api container to docker hub..."
 docker push stevobengtson/music-bingo-api:latest
+
+echo "Complete"
