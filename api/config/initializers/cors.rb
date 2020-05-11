24fbd9092ec56e111e22e758bbc1017bf2ffefa8
Steven Bengtson <steven.bengtson@me.com>
2020-05-11 12:46:00 -0700

    Update Gemfile

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -16,10 +16,10 @@
 # end
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'localhost:4200'
+        origins '*'
         resource '*',
         headers: :any,
         methods: %i(get post put patch delete options head),
-        expose: ['Link', 'Total', 'Per-Page']
+        expose: ['access-token', 'expiry', 'token-type', 'uid', 'client', 'Link', 'Total', 'Per-Page']
     end
 end
